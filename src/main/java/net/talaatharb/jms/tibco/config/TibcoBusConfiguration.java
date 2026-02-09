1cc14c7b1ac04cc9d14022d1bab7f83ad4ee08e9
Aceveera <v.rachayya.madapati@accenture.com>
2026-02-09 13:41:43 +0530

    initial code sample

---
+++ src/main/java/net/talaatharb/jms/tibco/config/TibcoBusConfiguration.java
@@ -0,0 +1,64 @@
+package net.talaatharb.jms.tibco.config;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.tibco.tibjms.TibjmsConnectionFactory;
+
+@Configuration
+@EnableJms
+public class TibcoBusConfiguration {
+	@Value("${ems.password}")
+	private String password;
+
+	@Value("${ems.port}")
+	private String port;
+
+	@Value("${ems.queue}")
+	private String queue;
+
+	@Value("${ems.server}")
+	private String server;
+
+	@Value("${ems.user}")
+	private String user;
+
+	@Bean(name = "jmsConnectionFactory")
+	public ConnectionFactory jmsConnectionFactory() throws JMSException {
+		final TibjmsConnectionFactory factory = new TibjmsConnectionFactory();
+
+		factory.setServerUrl(serverURL());
+		factory.setUserName(user);
+		factory.setUserPassword(password);
+
+		return factory;
+	}
+
+	@Bean
+	public JmsTemplate jmsTemplate(
+			@Autowired ConnectionFactory jmsConnectionFactory) {
+		final JmsTemplate jmsTemplate = new JmsTemplate();
+
+		jmsTemplate.setConnectionFactory(jmsConnectionFactory);
+		jmsTemplate.setDefaultDestinationName(queue);
+		jmsTemplate.setExplicitQosEnabled(true);
+		jmsTemplate.setDeliveryMode(DeliveryMode.PERSISTENT);
+		jmsTemplate.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+		jmsTemplate.setSessionTransacted(false);
+
+		return jmsTemplate;
+	}
+
+	private String serverURL() {
+		return "tcp://" + server + ":" + port;
+	}
+}
